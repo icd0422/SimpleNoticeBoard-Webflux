853359a03ca39c904c7693366257ec954a7ec9f9
cori <cori.jang@bunjang.co.kr>
2021-12-24 09:07:52 +0900

    Initial commit

---
+++ src/main/java/com/example/simplenoticeboardwebflux/SimpleNoticeBoardWebfluxApplication.java
@@ -0,0 +1,13 @@
+package com.example.simplenoticeboardwebflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleNoticeBoardWebfluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleNoticeBoardWebfluxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/simplenoticeboardwebflux/SimpleNoticeBoardWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simplenoticeboardwebflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleNoticeBoardWebfluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
